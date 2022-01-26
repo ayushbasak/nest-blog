bc15f907a1d20f5596e9549a0163f742d3e6f0ae
ayushbasak <ayushbasak0210@gmail.com>
2022-01-26 08:48:41 +0530

    Create Base template

---
+++ blog-backend/src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { BlogModule } from './blog/blog.module';
+ConfigModule.forRoot();
+@Module({
+  imports: [MongooseModule.forRoot(process.env.DB_URI), BlogModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
